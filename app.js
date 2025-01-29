9298bd6d385a487c3fc15885944f10881b58af1d
Christian Ngoc <christian180705@gmail.com>
2025-01-29 12:35:03 +0000

    task 10 get api/users

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const endPointsJson = require("./endpoints.json");
 const { getTopics } = require("./controllers/topicsController");
+const { getUsers } = require("./controllers/usersController");
 const {
   getArticle,
   getArticles,
   getArticleComment,
   postArticleComment,
-
   deleteComment,
-
   patchArticle,
-
 } = require("./controllers/articleController");
+
 const app = express();
 
 app.use(express.json());
@@ -26,15 +25,15 @@ app.get("/api/articles/:article_id", getArticle);
 
 app.get("/api/articles", getArticles);
 
+app.get("/api/users", getUsers);
+
 app.get("/api/articles/:article_id/comments", getArticleComment);
 
 app.post("/api/articles/:article_id/comments", postArticleComment);
 
-
 app.delete("/api/comments/:comment_id", deleteComment);
 
 app.patch("/api/articles/:article_id", patchArticle);
-
 
 app.get("*", (req, res) => {
   res.status(404).send({ msg: "Not found" });
