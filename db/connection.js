344419da7ebbe2148dda919dc137c06e304eac51
Christian Ngoc <christian180705@gmail.com>
2025-01-30 14:13:26 +0000

    task-14-hosting-application

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,6 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
+
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -12,7 +13,7 @@ if (ENV === "production") {
 }
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE not set");
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
