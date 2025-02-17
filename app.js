79d9a08c835650471d3a0feb2303fca0c15b8cd1
Christian Ngoc <christian180705@gmail.com>
2025-02-17 10:20:50 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const endPointsJson = require("./endpoints.json");
 const apiRouter = require("./routes/api-router");
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
